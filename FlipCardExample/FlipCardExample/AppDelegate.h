17cbfd87bbe8c542bf347cb4c24d0fc17b96fe2e
Adam Jacaruso <ajacaruso@gmail.com>
2013-01-21 15:22:29 -0700

    Adding Base Files

---
+++ FlipCardExample/FlipCardExample/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  FlipCardExample
+//
+//  Created by Adam Jacaruso on 1/21/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
