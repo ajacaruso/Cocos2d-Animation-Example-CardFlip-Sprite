215e4eadf0becbb75fe8729d8ec3d2677565eed5
Adam Jacaruso <ajacaruso@gmail.com>
2013-01-21 15:47:36 -0700

    Adding Demo Images and Flip Card Sprite

---
+++ FlipCardExample/FlipCardExample/SpriteFlipCard.h
@@ -0,0 +1,16 @@
+//
+//  SpriteFlipCard.h
+//  FlipCardExample
+//
+//  Created by Adam Jacaruso on 1/21/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface SpriteFlipCard : CCSprite {
+    
+}
+
+@end
---
+++ FlipCardExample/FlipCardExample/SpriteFlipCard.m
@@ -0,0 +1,14 @@
+//
+//  SpriteFlipCard.m
+//  FlipCardExample
+//
+//  Created by Adam Jacaruso on 1/21/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "SpriteFlipCard.h"
+
+
+@implementation SpriteFlipCard
+
+@end
